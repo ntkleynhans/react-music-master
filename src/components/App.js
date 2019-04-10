5083bd988324a5d75bab1135822f1283f0293e0b
Neil Kleynhans <ntkleynhans@gmail.com>
2019-04-10 16:53:24 +0200

    basic event layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = { artistQuery: '' };
+
+  updateArtistQuery = event => {
+    this.setState({ artistQuery: event.target.value })
+  }
+
+  searchArtist = () => {
+
+  }
+
+  handleKeyPress = event => {
+    if('Enter' == event.key) {
+      this.searchArtist();
+    }
+  }
+
   render() {
     return(
-      <div>React App</div>
+      <div>
+        <h2>Music Master</h2>
+        <input onChange={this.updateArtistQuery}
+              onKeyPress={this.handleKeyPress}
+              placeholder='Search for an artist'/>
+        <button type="button" className="btn btn-info" onClick={this.searchArtist}>Search</button>
+      </div>
     );
   }
 }
