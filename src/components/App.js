98333bf23b16212727dd3925a92b9ae02e11436d
Neil Kleynhans <ntkleynhans@gmail.com>
2019-04-10 17:33:57 +0200

    api calls for artist and top 10 tracks -- save in state

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
 
+const API_URL = 'https://spotify-api-wrapper.appspot.com/artist';
+
 class App extends Component {
-  state = { artistQuery: '' };
+  state = { artistQuery: '', artist: null, topTracks: null };
 
   updateArtistQuery = event => {
     this.setState({ artistQuery: event.target.value })
   }
 
   searchArtist = () => {
+    const artistName = this.state.artistQuery;
+    fetch(`${API_URL}/${artistName}`)
+    .then(response => response.json())
+    .then(json => {
+      if(json.artists.total > 1) {
+        this.setState({ artist : json.artists.items[0]});
+        this.fetchTopTracks();
+      }
+    })
+    .catch(error => alert(error.message));
+  }
 
+  fetchTopTracks = () => {
+    const artistId = this.state.artist.id;
+    fetch(`${API_URL}/${artistId}/top-tracks`)
+    .then(response => response.json())
+    .then(json => {
+      this.setState({ topTracks : json.tracks})
+    })
+    .catch(error => alert(error.message));
   }
 
   handleKeyPress = event => {
