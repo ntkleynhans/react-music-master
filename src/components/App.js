2a13595495a279bf59064901f9f9f3bdbd1268b2
Neil Kleynhans <ntkleynhans@gmail.com>
2019-04-11 16:05:39 +0200

    refactor search into its own component

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
+import Search from './Search';
 import Artist from './Artist';
 import Tracks from './Tracks';
 
 const API_URL = 'https://spotify-api-wrapper.appspot.com/artist';
 
 class App extends Component {
-  state = { artistQuery: '', artist: null, tracks: [] };
+  state = { artist: null, tracks: [] };
 
-  updateArtistQuery = event => {
-    this.setState({ artistQuery: event.target.value })
-  }
-
-  searchArtist = () => {
-    const artistName = this.state.artistQuery;
+  searchArtist = (artistName) => {
     fetch(`${API_URL}/${artistName}`)
     .then(response => response.json())
     .then(json => {
@@ -34,20 +30,11 @@ class App extends Component {
     .catch(error => alert(error.message));
   }
 
-  handleKeyPress = event => {
-    if('Enter' == event.key) {
-      this.searchArtist();
-    }
-  }
-
   render() {
     return(
       <div>
         <h2>Music Master</h2>
-        <input onChange={this.updateArtistQuery}
-              onKeyPress={this.handleKeyPress}
-              placeholder='Search for an artist'/>
-        <button type="button" className="btn btn-info" onClick={this.searchArtist}>Search</button>
+        <Search searchArtist={this.searchArtist}/>
         <Artist artist={this.state.artist} />
         <Tracks tracks={this.state.tracks} />
       </div>
